bff2a7a2bdff7266bff48527f51be7cc5806343c
adrian.weselak <adrian.weselak31@gmail.com>
2022-05-08 21:20:41 +0200

    szablon

---
+++ src/main/java/com/example/ak2project/Ak2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ak2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ak2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ak2ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ak2project/Ak2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ak2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ak2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
